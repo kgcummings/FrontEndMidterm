6ea08af3a16375f6614761804b26c14f5743c9ef
kgcummings <113394583+kgcummings@users.noreply.github.com>
2022-11-07 17:06:53 -0500

    Branch Java Edits

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("myBtn").addEventListener("click", buttonAlert);
+
+function buttonAlert() {
+  alert ("Hi! Isn't Javascript cool-???");
+}
+
+function helloAnimation(){
+    alert ("Oh! You found a secret!");
+    document.getElementById('hello').style.transform="scale(1.1,1.1)"
+}
